085e6122df2f9a754ade7c6d6d9c23af8e435761
Ian Henderson <ian.henderson1215@gmail.com>
2023-05-11 12:17:01 -0700

    first commit

---
+++ src/main/java/com/ihendy12/tempConverter/TempConverterApplication.java
@@ -0,0 +1,13 @@
+package com.ihendy12.tempConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihendy12/tempConverter/TempConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihendy12.tempConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
